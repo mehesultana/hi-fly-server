76ad82addc37dd6a20680d9a869ceb07c2e7cfc1
mehesultana <sultanamehe01@gmail.com>
2022-05-20 20:59:23 +0600

    connect mongodb

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
 const app = express();
+dotenv.config();
+
+const connect = async () => {
+	try {
+		await mongoose.connect(process.env.MONGO);
+		console.log('MONGO connect');
+	} catch (error) {
+		throw errow;
+	}
+};
 
 app.listen(5000, () => {
+	connect();
 	console.log('connect!!');
 });
