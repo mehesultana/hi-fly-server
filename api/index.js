26c83451b63e1f8518d177272731b0ae41b202a1
mehesultana <sultanamehe01@gmail.com>
2022-05-23 23:27:21 +0600

    Node.js MongoDB CRUD Operations

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import authRoute from './routes/auth.js';
+import usersRoute from './routes/users.js';
+import hotelsRoute from './routes/hotels.js';
+import roomsRoute from './routes/rooms.js';
 
 const app = express();
 dotenv.config();
@@ -17,13 +21,21 @@ const connect = async () => {
 mongoose.connection.on('disconnected', () => {
 	console.log('mongodb disconnected');
 });
-mongoose.connection.on('connected', () => {
-	console.log('mongodb connected');
-});
 
-app.get('/', (req, res) => {
-	res.send('hellooooooooo');
-});
+// mongoose.connection.on('connected', () => {
+// 	console.log('mongodb connected');
+// });
+
+// app.get('/', (req, res) => {
+// 	res.send('hellooooooooo');
+// });
+
+//middlewares
+
+app.use('/api/auth', authRoute);
+app.use('/api/users', authRoute);
+app.use('/api/hotels', authRoute);
+app.use('/api/rooms', authRoute);
 
 app.listen(5000, () => {
 	connect();
