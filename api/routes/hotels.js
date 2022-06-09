8878a27e4b85c330e1eee1254c515521be9c51b7
mehesultana <sultanamehe01@gmail.com>
2022-06-09 21:19:58 +0600

    Node MongoDB Authentication (Login / Register)

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,21 +1,11 @@
 import express from 'express';
+import { createHotel } from '../controllers/hotel.js';
 import Hotel from '../models/Hotel.js';
-import { createError } from '../utils/error.js';
 
 const router = express.Router();
 
 //CREATE
-router.post('/', async (req, res) => {
-	const newHotel = new Hotel(req.body);
-
-	try {
-		const savedHotel = await newHotel.save();
-		res.status(200).json(savedHotel);
-		console.log(savedHotel);
-	} catch (err) {
-		res.status(500).json(err);
-	}
-});
+router.post('/', createHotel);
 
 //UPDATE
 router.put('/:id', async (req, res) => {
