7a30482f3568a804af0712aba6984749d980ea90
mehesultana <sultanamehe01@gmail.com>
2022-06-09 21:10:18 +0600

    Node.js Express Error Handling

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import Hotel from '../models/Hotel.js';
+import { createError } from '../utils/error.js';
 
 const router = express.Router();
 
@@ -51,11 +52,8 @@ router.get('/:id', async (req, res) => {
 //GET ALL
 
 router.get('/', async (req, res, next) => {
-	const failed = true;
-	const err = new Error();
-	err.status = 404;
-	err.message = 'Sorry not found';
-	if (failed) return next(err);
+	// const failed = true;
+	//  if (failed) return next(createError(401, 'You are not authenticated'));
 
 	try {
 		const hotels = await Hotel.find();
