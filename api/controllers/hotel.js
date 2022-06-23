6cd355398a913040b83cf381a642e899a007f88e
mehesultana <sultanamehe01@gmail.com>
2022-06-23 19:51:02 +0600

    React Fetch Data from Node Express API

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -11,6 +11,7 @@ export const createHotel = async (req, res, next) => {
 		next(err);
 	}
 };
+
 export const updateHotel = async (req, res, next) => {
 	try {
 		const updatedHotel = await Hotel.findByIdAndUpdate(
@@ -23,6 +24,7 @@ export const updateHotel = async (req, res, next) => {
 		next(err);
 	}
 };
+
 export const deleteHotel = async (req, res, next) => {
 	try {
 		await Hotel.findByIdAndDelete(req.params.id);
@@ -31,6 +33,7 @@ export const deleteHotel = async (req, res, next) => {
 		next(err);
 	}
 };
+
 export const getHotel = async (req, res, next) => {
 	try {
 		const hotel = await Hotel.findById(req.params.id);
@@ -39,10 +42,44 @@ export const getHotel = async (req, res, next) => {
 		next(err);
 	}
 };
+
 export const getHotels = async (req, res, next) => {
 	try {
 		const hotels = await Hotel.find();
 		res.status(200).json(hotels);
+	} catch (err) {
+		next(err);
+	}
+};
+
+export const countByCity = async (req, res, next) => {
+	const cities = req.query.cities.split(',');
+	try {
+		const list = await Promise.all(
+			cities.map((city) => {
+				return Hotel.countDocuments({ city: city });
+			})
+		);
+		res.status(200).json(list);
+	} catch (err) {
+		next(err);
+	}
+};
+export const countByType = async (req, res, next) => {
+	try {
+		const hotelCount = await Hotel.countDocuments({ type: 'hotel' });
+		const apartmentCount = await Hotel.countDocuments({ type: 'apartment' });
+		const resortCount = await Hotel.countDocuments({ type: 'resort' });
+		const villaCount = await Hotel.countDocuments({ type: 'villa' });
+		const cabinCount = await Hotel.countDocuments({ type: 'cabin' });
+
+		res.status(200).json([
+			{ type: 'hotel', count: hotelCount },
+			{ type: 'apartments', count: apartmentCount },
+			{ type: 'resorts', count: resortCount },
+			{ type: 'villas', count: villaCount },
+			{ type: 'cabins', count: cabinCount },
+		]);
 	} catch (err) {
 		next(err);
 	}
