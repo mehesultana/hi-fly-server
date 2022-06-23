6f601eb086b9903f99c0d7f003b9184eebf8e754
mehesultana <sultanamehe01@gmail.com>
2022-06-23 22:56:29 +0600

    React Search using a Backend API

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import './Navbar.css';
+import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
 	return (
-		<div className="navbar">
-			<div className="navContainer">
-				<span className="logo">Hi Fly</span>
-				<div className="navItems">
-					<button className="navButton">Register</button>
-					<button className="navButton">Login</button>
+		<div className='navbar'>
+			<div className='navContainer'>
+				<Link to='/' style={{ color: 'inherit', textDecoration: 'none' }}>
+					<span className='logo'>Hi Fly</span>
+				</Link>
+				<div className='navItems'>
+					<button className='navButton'>Register</button>
+					<button className='navButton'>Login</button>
 				</div>
 			</div>
 		</div>
