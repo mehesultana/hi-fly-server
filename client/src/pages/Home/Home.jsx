141cbbe9ef91695423a011b2accfdf817b24ca1f
mehesultana <sultanamehe01@gmail.com>
2022-06-23 20:12:45 +0600

    React Fetch Data from Node Express API

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -13,11 +13,11 @@ export const Home = () => {
 		<div>
 			<Navbar />
 			<Header />
-			<div className="homeContainer">
+			<div className='homeContainer'>
 				<Featured />
-				<h1 className="homeTitle">Browse by property type</h1>
+				<h1 className='homeTitle'>Browse by property type</h1>
 				<PropertyList />
-				<h1 className="homeTitle">Homes guests love</h1>
+				<h1 className='homeTitle'>Homes guests love</h1>
 				<FeaturedProperties />
 				<MailList />
 				<Footer />
