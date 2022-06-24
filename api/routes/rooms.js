35b0bc07a76e908f8c8a0d3fba99691f3afc0463
mehesultana <sultanamehe01@gmail.com>
2022-06-25 00:49:10 +0600

    Reach Fetch using URL Location

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -14,7 +14,7 @@ const router = express.Router();
 router.post('/:hotelid', verifyAdmin, createRoom);
 
 //UPDATE
-// router.put('/availability/:id', updateRoomAvailability);
+router.put('/availability/:id', updateRoomAvailability);
 router.put('/:id', verifyAdmin, updateRoom);
 //DELETE
 router.delete('/:id/:hotelid', verifyAdmin, deleteRoom);
