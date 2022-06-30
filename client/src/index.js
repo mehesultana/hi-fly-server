b3b7f37b92db96e73029cf9162f58b7d008f720e
mehesultana <sultanamehe01@gmail.com>
2022-07-01 00:22:21 +0600

    Login Auth Context API

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { AuthContextProvider } from './context/AuthContext';
 import { SearchContextProvider } from './context/SearchContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<BrowserRouter>
-		<React.StrictMode>
-			<SearchContextProvider>
-				<App />
-			</SearchContextProvider>
-		</React.StrictMode>
+		<AuthContextProvider>
+			<React.StrictMode>
+				<SearchContextProvider>
+					<App />
+				</SearchContextProvider>
+			</React.StrictMode>
+		</AuthContextProvider>
 	</BrowserRouter>
 );
